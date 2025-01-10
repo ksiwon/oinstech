87589e28312cbb23abbee070334a9e0557980be4
jhaana <hjang577@gmail.com>
2025-01-10 22:50:54 +0900

    final-login

--- backend/src/index.js
+++ backend/src/index.js
@@ -24,14 +24,14 @@ app.get('/signup', (req, res) => {
     res.render('signup')
 })
 app.get('/', (req, res) => {
-    res.render('login')
+    res.render('login') 
 })
 
 
 
-// app.get('/home', (req, res) => {
-//     res.render('home')
-// })
+app.get('/home', (req, res) => {
+    res.render('home')
+})
 
 app.post('/signup', async (req, res) => {
     
@@ -47,6 +47,7 @@ app.post('/signup', async (req, res) => {
     }
 
     await LogInCollection.insertMany([data])
+    res.status(221).render("home", { naming: `${req.body.password}+${req.body.name}` })
 
 
 //     const checking = await LogInCollection.findOne({ name: req.body.name })
