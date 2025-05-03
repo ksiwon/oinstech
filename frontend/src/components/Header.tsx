a90cd3df16655891ad21579ba8ceaaa8123d81b8
ksiwon <pjo12346@kaist.ac.kr>
2025-05-03 20:18:42 +0900

    oins new

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,93 +1,24 @@
 import React from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { RootState } from "../redux/store";
-import { clearUserData } from "../redux/slices/userSlice";
 import {
   HeaderContainer,
   LogoAndNav,
   Logo,
-  Nav,
-  NavItem,
-  AuthSection,
-  AuthButton,
-  UserSection,
-  UserName,
   LogoText
 } from "./Header.styles";
 import { useNavigate } from "react-router-dom";
-import { FaTree } from "react-icons/fa";
 
 const Header: React.FC = () => {
   const navigate = useNavigate();
-  const dispatch = useDispatch();
-
-  // Redux 상태 가져오기
-  const { role, studentData, teacherData } = useSelector((state: RootState) => state.user);
-  const isLoggedIn = role !== null;
-  const username = role === "student" ? studentData?.name : teacherData?.name;
-  const userId = role === "student" ? studentData?.id : teacherData?.id;
-
   // 네비게이션 핸들러
   const handleNavClick = (path: string) => navigate(path);
-
-  const handleChat = () => {
-    navigate("/chat/"+userId);
-  };
-
-  // 로그아웃 핸들러
-  const handleLogout = () => {
-    navigate("/");
-    dispatch(clearUserData());
-  };
-
-  const handleStudentLogin = () => {
-    navigate("/login/student");
-  }
-
-  const handleTeacherLogin = () => {
-    navigate("/login/teacher");
-  }
-
-  const handleMypage = () => {
-    navigate("/mypage");
-  }
-
   return (
     <HeaderContainer>
       {/* 로고 및 네비게이션 */}
       <LogoAndNav>
         <Logo onClick={() => handleNavClick("/")}>
-          <FaTree size="36px"/>
-          <LogoText>과수원</LogoText>
+          <LogoText>OINS</LogoText>
         </Logo>
-        {isLoggedIn && (role === "teacher") && (
-          <Nav>
-            <NavItem onClick={() => handleNavClick("/search/student")}>학생 찾기</NavItem>
-            <NavItem onClick={() => handleNavClick("/mygroup")}>그룹 매칭</NavItem>
-            <NavItem onClick={handleChat}>채팅</NavItem>
-          </Nav>
-        )}
-        {isLoggedIn && (role === "student") && (
-          <Nav>
-            <NavItem onClick={() => handleNavClick("/search/teacher")}>강사 찾기</NavItem>
-            <NavItem onClick={() => handleNavClick("/search/group")}>그룹 매칭</NavItem>
-            <NavItem onClick={handleChat}>채팅</NavItem>
-          </Nav>
-        )}
       </LogoAndNav>
-
-      {/* 인증 섹션 */}
-      {isLoggedIn ? (
-        <UserSection>
-          <UserName onClick={handleMypage}>{username} 님</UserName>
-          <AuthButton onClick={handleLogout}>Log Out</AuthButton>
-        </UserSection>
-      ) : (
-        <AuthSection>
-          <AuthButton onClick={handleStudentLogin}>학생 Login</AuthButton>
-          <AuthButton onClick={handleTeacherLogin}>강사 Login</AuthButton>
-        </AuthSection>
-      )}
     </HeaderContainer>
   );
 };
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,36 +2,24 @@ import React from "react";
 import styled, { keyframes } from "styled-components";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import homeBg from "../assets/homeBg.png";
-import { useSelector } from "react-redux";
-import { RootState } from "../redux/store";
-import { useNavigate } from "react-router-dom";
-import { FaTree } from "react-icons/fa";
 
 const Home: React.FC = () => {
-  const { role, studentData, teacherData } = useSelector((state: RootState) => state.user);
-  const userId = role === "student" ? studentData?.id : teacherData?.id;
-  const navigate = useNavigate();
-  const handleNavClick = (path: string) => navigate(path);
-
   const lines = [
-    "과외 플랫폼의 새로운 패러다임",
-    "성공적인 여정을 위한 최고의 커넥션",
-    "과수원",
-    "학생과 튜터, 꿈과 현실을 잇는 플랫폼",
-    "내게 맞는 튜터와 함께, 미래로 가는 한 걸음",
+    "문제 업로드 / AI 문제 제작",
+    "해설지 맞춤형 답안지 자동 제작",
+    "OINS",
+    "자체 제작 OCR 기반 빠른 채점",
+    "AI 기반 통계 + 피드백 관리",
   ];
 
   return (
     <GlobalWrapper>
       <Header />
-      {role === null ? (
-        // Logout 상태
         <Main>
           {lines.map((line, index) => (
             <LineContainer key={`line-${index}`} isLogo={line === "과수원"}>
-              {line === "과수원" ? (
-                <Logo><FaTree size="140px"/><LogoText>{line}</LogoText></Logo>
+              {line === "OINS" ? (
+                <Logo><LogoText>{line}</LogoText></Logo>
               ) : (
                 <MovingText>
                   <TextContent>
@@ -47,57 +35,6 @@ const Home: React.FC = () => {
             </LineContainer>
           ))}
         </Main>
-      ) : ( role === "student" ? (
-        // Student Login 상태
-        <MainLoggedIn>
-          <TextButtonContainer>
-            <TextContainer>
-              <MainTitle>과수원</MainTitle>
-              <Subtitle>내게 맞는 튜터와 함께, 미래로 가는 한 걸음</Subtitle>
-            </TextContainer>
-            <ButtonContainer>
-              <ActionButton color="green" onClick={() => handleNavClick("/search/teacher")}>
-                <Icon className="fas fa-search" />
-                <ButtonText>강사 찾기</ButtonText>
-              </ActionButton>
-              <ActionButton color="turkey" onClick={() => handleNavClick("/search/group")}>
-                <Icon className="fas fa-users" />
-                <ButtonText>그룹 매칭</ButtonText>
-              </ActionButton>
-              <ActionButton color="blue" onClick={() => handleNavClick("/chat/"+userId+"/poiu")}>
-                <Icon className="fas fa-comments" />
-                <ButtonText>채팅</ButtonText>
-              </ActionButton>
-            </ButtonContainer>
-          </TextButtonContainer>
-          <ImageContainer />
-        </MainLoggedIn>
-      ) : (
-        // Teacher Login 상태
-        <MainLoggedIn>
-          <TextButtonContainer>
-            <TextContainer>
-              <MainTitle>과수원</MainTitle>
-              <Subtitle>내게 맞는 튜터와 함께, 미래로 가는 한 걸음</Subtitle>
-            </TextContainer>
-            <ButtonContainer>
-              <ActionButton color="green" onClick={() => handleNavClick("/search/student")}>
-                <Icon className="fas fa-search" />
-                <ButtonText>학생 찾기</ButtonText>
-              </ActionButton>
-              <ActionButton color="turkey" onClick={() => handleNavClick("/mygroup")}>
-                <Icon className="fas fa-users" />
-                <ButtonText>그룹 매칭</ButtonText>
-              </ActionButton>
-              <ActionButton color="blue" onClick={() => handleNavClick("/chat/"+userId)}>
-                <Icon className="fas fa-comments" />
-                <ButtonText>채팅</ButtonText>
-              </ActionButton>
-            </ButtonContainer>
-          </TextButtonContainer>
-          <ImageContainer />
-        </MainLoggedIn>
-      ))}
       <Footer />
     </GlobalWrapper>
   );
@@ -194,90 +131,4 @@ const LogoText = styled.div`
   font-family: Pretendard, sans-serif;
   font-size: 160px;
   font-weight: 900;
-`;
-
-// Login 상태의 Main 컨테이너
-const MainLoggedIn = styled.main`
-  flex: 1;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  background-size: cover;
-`;
-
-// 텍스트 버튼 컨테이너
-const TextButtonContainer = styled.div`
-  display: flex;
-  gap: 80px;
-  flex-direction: column;
-  align-items: center;
-  padding: 0 48px;
-`;
-
-// 이미지 컨테이너
-const ImageContainer = styled.div`
-  width: 100%;
-  height: 100%;
-  background: url(${homeBg}) no-repeat center center;
-  background-size: cover;
-`;
-
-// Login 상태의 텍스트 컨테이너
-const TextContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  text-align: center;
-  gap: 16px;
-`;
-
-const MainTitle = styled.div`
-  font-size: 100px;
-  font-weight: 900;
-  color: ${({ theme }) => theme.colors.primary};
-  text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-  margin: 0;
-`;
-
-const Subtitle = styled.div`
-  font-size: ${({ theme }) => theme.typography.T2.fontSize};
-  font-weight: ${({ theme }) => theme.typography.T2.fontWeight};
-  color: ${({ theme }) => theme.colors.primary};
-  margin: 0;
-`;
-
-// Login 상태의 버튼 컨테이너
-const ButtonContainer = styled.div`
-  display: flex;
-  gap: 16px;
-`;
-
-const ActionButton = styled.button<{ color: "green" | "turkey" | "blue" }>`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  width: 128px;
-  height: 128px;
-  padding: 8px;
-  border-radius: 16px;
-  background-color: ${({ theme, color }) => theme.colors[color][600]};
-  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.25);
-  border: none;
-  cursor: pointer;
-  gap: 16px;
-
-  &:hover {
-    box-shadow: 0px 6px 12px rgba(0, 0, 0, 0.3);
-  }
-`;
-
-const Icon = styled.i`
-  font-size: 48px;
-  color: ${({ theme }) => theme.colors.white};
-`;
-
-const ButtonText = styled.span`
-  font-size: ${({ theme }) => theme.typography.T4.fontSize};
-  font-weight: ${({ theme }) => theme.typography.T4.fontWeight};
-  color: ${({ theme }) => theme.colors.white};
 `;
